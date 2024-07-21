98f31975541eb79679ef65489c3af1131cdab939
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-07-21 11:32:38 -0700

    env for api base url

--- frontend/src/common/api.ts
+++ frontend/src/common/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const API_BASE_URL =
+    import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }
