c42c642b398da6c46565ea287b0032ba04466312
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-07-20 20:29:50 -0700

    login/signup api calls

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -3,33 +3,70 @@ import { useForm } from '@mantine/form';
 import { z } from 'zod';
 import { zodResolver } from 'mantine-form-zod-resolver';
 import { passwordSchema } from '../common/helpers';
+import apiClient from '../common/api';
+import { useMutation } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
 
 const schema = z.object({
-    email: z.string().email({ message: 'Invalid email address' }),
+    username: z.string().min(5).max(20),
     password: passwordSchema
 });
+
+type LoginSchema = z.infer<typeof schema>;
 
 export default function LoginForm() {
     const form = useForm({
         initialValues: {
-            email: '',
+            username: '',
             password: ''
         },
         validate: zodResolver(schema)
     });
 
+    const mutation = useMutation({
+        mutationFn: (values: LoginSchema) =>
+            apiClient.post<{ token: string }>('/login', values),
+        onSuccess: (data) => {
+            console.log(data);
+        }
+    });
+
+    const handleSubmit = async (values: LoginSchema) => {
+        try {
+            await mutation.mutateAsync(values);
+        } catch (err) {
+            if (isAxiosError(err)) {
+                if (err.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else if (err.request) {
+                    // The request was made but no response was received
+                    // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(err.request);
+                } else {
+                    // Something happened in setting up the request that triggered an err
+                    console.log('err', err.message);
+                }
+            }
+        }
+    };
+
     return (
         <form
             onSubmit={form.onSubmit((values) => {
-                console.log(values);
+                handleSubmit(values as LoginSchema);
             })}
         >
             <TextInput
                 withAsterisk
-                label='Email'
-                placeholder='your@email.com'
-                key={form.key('email')}
-                {...form.getInputProps('email')}
+                label='Username'
+                placeholder='username'
+                key={form.key('username')}
+                {...form.getInputProps('username')}
             />
             <TextInput
                 withAsterisk
--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -3,10 +3,13 @@ import { useForm } from '@mantine/form';
 import { z } from 'zod';
 import { zodResolver } from 'mantine-form-zod-resolver';
 import { passwordSchema } from '../common/helpers';
+import { useMutation } from '@tanstack/react-query';
+import apiClient from '../common/api';
+import { isAxiosError } from 'axios';
 
 const schema = z
     .object({
-        email: z.string().email({ message: 'Invalid email address' }),
+        username: z.string().min(5).max(20),
         password: passwordSchema,
         confirmPassword: z.string()
     })
@@ -15,28 +18,65 @@ const schema = z
         path: ['confirmPassword']
     });
 
+type SignupSchema = z.infer<typeof schema>;
+
 export default function SignupForm() {
     const form = useForm({
         initialValues: {
-            email: '',
+            username: '',
             password: '',
             confirmPassword: ''
         },
         validate: zodResolver(schema)
     });
 
+    const mutation = useMutation({
+        mutationFn: (values: SignupSchema) =>
+            apiClient.post<{ token: string }>('/signUp', {
+                username: values.username,
+                password: values.password
+            }),
+        onSuccess: (data) => {
+            console.log(data);
+        }
+    });
+
+    const handleSubmit = async (values: SignupSchema) => {
+        try {
+            await mutation.mutateAsync(values);
+        } catch (err) {
+            if (isAxiosError(err)) {
+                if (err.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else if (err.request) {
+                    // The request was made but no response was received
+                    // `err.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(err.request);
+                } else {
+                    // Something happened in setting up the request that triggered an err
+                    console.log('err', err.message);
+                }
+            }
+        }
+    };
+
     return (
         <form
             onSubmit={form.onSubmit((values) => {
-                console.log(values);
+                handleSubmit(values);
             })}
         >
             <TextInput
                 withAsterisk
-                label='Email'
-                placeholder='your@email.com'
-                key={form.key('email')}
-                {...form.getInputProps('email')}
+                label='Username'
+                placeholder='username'
+                key={form.key('username')}
+                {...form.getInputProps('username')}
             />
             <TextInput
                 withAsterisk
