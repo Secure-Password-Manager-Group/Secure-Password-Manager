22a94a87c8461b908da452492146096cc369101a
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-07-21 11:25:22 -0700

    logout added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,21 @@
 import { Group, Button, Box } from '@mantine/core';
-import { useLocation } from 'react-router-dom';
+import { useAuthStore } from '../store/auth';
 
 export default function Navbar() {
-    const location = useLocation();
+    const { token, clearAuth } = useAuthStore();
+
+    const handleLogout = () => {
+        clearAuth();
+        localStorage.removeItem('token');
+    };
 
     return (
         <Box bg='red' px='lg' h='50px'>
             <Group justify='space-between' h='100%'>
                 <div>Secure Password Manager</div>
-                {location.pathname === '/dashboard' && (
+                {token && (
                     <Group>
-                        <Button>Logout</Button>
+                        <Button onClick={() => handleLogout()}>Logout</Button>
                     </Group>
                 )}
             </Group>
